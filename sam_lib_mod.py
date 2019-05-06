3a12ee5101643bcd126cdc899aebc40de9bc8ada
vu <buianhvu.hust@gmail.com>
2019-05-06 22:38:17 +0700

    test

--- sam_lib_mod.py
+++ sam_lib_mod.py
@@ -121,7 +121,7 @@ def mda_z(xx, gg, Z, noise, lambda_, alpha, beta, V):
 	print("Shape id_mat {}".format(type(id_mat)))
 	#tills converges
 	print("Converging")
-	for converge in range(4):
+	for converge in range(2):
 		print("Converge : {}".format(converge))
 		W = compute_gg_inve(G, beta, lambda_).dot(M) #update W
 		print("W type {} Wshape {}".format(type(W), W.shape))
