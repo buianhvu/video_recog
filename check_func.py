6436fe5dbb57f5f0c7d0f1c026b7110ea9d6d435
vu <buianhvu.hust@gmail.com>
2019-05-07 21:37:47 +0700

    change type of test from float to labels string

--- check_func.py
+++ check_func.py
@@ -1,17 +1,40 @@
+# import numpy as np
+
+# a = np.ones((4,4), int)
+# b = [5,8,7,1] #d=4
+# print("b checks")
+# print b[0:3]
+
+
+# print a
+# np.fill_diagonal(a, b)
+# print a
+# print np.diag(a)
+
+# print("reg")
+# reg = np.identity(4+1)
+# reg[4,4] = 6
+# print
 import numpy as np
+from sklearn.svm import SVC
 
-a = np.ones((4,4), int)
-b = [5,8,7,1] #d=4
-print("b checks")
-print b[0:3]
+yy = np.ones((3,))
+yy_arr = []
+for y in yy:
+	yy_arr.append(str(y))
+print (type(yy[1]))
+print (type(yy_arr[1]))
+print yy_arr
+print yy
+print("yy shape {}".format(yy.shape))
 
+x_train = np.random.rand(3,5)
+y_train = ['a','b','c']
+xx_test = np.random.rand(3,5)
+yy_test = ['a','b','c']
 
-print a
-np.fill_diagonal(a, b)
-print a
-print np.diag(a)
+clf = SVC(gamma='auto')
+clf.fit(x_train, y_train)
 
-print("reg")
-reg = np.identity(4+1)
-reg[4,4] = 6
-print reg
+score = clf.score(xx_test, yy_test)
+print ("Score {}".format(score))
--- test.py
+++ test.py
@@ -60,9 +60,12 @@ bias_train = np.ones((1,n))
 x_train = np.concatenate((xx1,bias_train), axis = 0)
 x_train = (W.dot(x_train)).transpose()
 y_train = yy1.reshape(yy1.shape[1],)
+yy_train_arr = []
+for y in y_train:
+	yy_train_arr.append(str(y))
 # xx = np.concatenate((xx,np.ones((1,xx.shape[1]))), axis = 0)
 
-clf.fit(x_train, y_train)
+clf.fit(x_train, yy_train_arr)
 
 biases = np.ones((1, n))
 #supposed tested on xx2
@@ -70,8 +73,11 @@ x_test = np.concatenate((xx2,biases), axis=0) #(d+1)xn
 x_test = W.dot(x_test) #(d+1, n)
 x_test = x_test.transpose() #feed to svm
 y_test = yy2.reshape(yy2.shape[1],)
+yy_test_arr = []
+for yt in y_test:
+	yy_test_arr.append(str(yt))
 
-score = clf.score(x_test, y_test)
+score = clf.score(x_test, yy_test_arr)
 print("calculating score ...")
 print("Score train on , test on X2: {}".format(score))
 
