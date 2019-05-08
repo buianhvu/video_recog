d2b3aadb0acced078deeea4fe0008a6461071151
vu <buianhvu.hust@gmail.com>
2019-05-08 12:33:33 +0700

    fix cal Z

--- test1_sam.py
+++ test1_sam.py
@@ -70,7 +70,7 @@ print("Initializing classifier: ")
 clf = KNeighborsClassifier(n_neighbors=1)
 
 x_train = hx_xx[:,0:2*n].transpose()
-y_train = yy1.reshape(yy1.shape[1],).astype(str)
+y_train = [yy1,yy2].reshape(660,).astype(str)
 
 clf.fit(x_train, y_train)
 
