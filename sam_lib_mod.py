67909f921ae04779c83e45550440f18733d7d3d9
vu <buianhvu.hust@gmail.com>
2019-05-09 15:36:17 +0700

    add loss function

--- sam_lib_mod.py
+++ sam_lib_mod.py
@@ -162,7 +162,7 @@ def mda_z(xx, gg, Z, noise, lambda_, alpha, beta, V, Converge):
 			G[view] = inve_W_to_G.dot(temp) #dx(d+1)
 			print("End updating G{}".format(view))
 		t_1 = time.time() 
-		print ("Loss at {}: {}".format(converge,find_loss(W, q, zz, Z, 5, G, GG,alpha, beta)))
+		print ("Loss at {}: {}".format(converge,find_loss(W, q, xx, Z, 5, G, GG,alpha, beta)))
 		t_2 = time.time()
 		print("time cal loss: {}".format(t_2-t_1))
 
